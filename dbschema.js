a091303b97941a8b21f699b32bed0aeaf0e0e6b3
Erika Miwa <erikamiwa612@gmail.com>
2020-05-03 01:37:02 -0700

    add schema

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  comments: [
+    {
+      userHandle: "user",
+      body: "this is the comment body",
+      createdAt: '2020-05-01T11:46:01.018Z',
+      likeCount: 5,
+      commentCount: 3
+    }
+  ]
+}
