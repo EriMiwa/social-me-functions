f4eec61b9e259b281eaec96555ab7d6f43f62fb4
Erika Miwa <erikamiwa612@gmail.com>
2020-05-12 23:23:24 -0700

    refuctaring

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,15 @@ const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
-const { getAllComments, postOneComment } = require('./handlers/comments')
-const { signup, login } = require('./handlers/users')
+const { 
+  getAllComments, 
+  postOneComment 
+} = require('./handlers/comments')
+
+const { 
+  signup, 
+  login 
+} = require('./handlers/users')
 
 
 app.get('/comments', getAllComments);
